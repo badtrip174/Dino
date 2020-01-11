29ce713f4c3eee731f8a788a113183421e3c3a7b
Vyacheslav Galitovskij <Galtovskij2000@gmail.com>
2020-01-11 21:28:40 +0500

    Dino

--- Dino.py
+++ Dino.py
@@ -11,6 +11,23 @@ pygame.display.set_caption("DinoGame!")
 icon = pygame.image.load('Backgrounds/icon.png')
 pygame.display.set_icon(icon)
 
+class Cactus:
+	def __init__(self, x, y, width, height, speed):
+		self.x = x 
+		self.y = y 
+		self.width = width
+		self.height = height
+		self.speed = speed
+
+	def move(self):
+		if self.x >= -self.width:
+			pygame.draw.rect(display, (0, 255, 0), (self.x, self.y, self.width, self.height))
+			self.x -= self.speed
+
+		else:
+			self.x = display_width - 50
+
+
 usr_width = 60
 usr_height = 100
 usr_x = 20 
@@ -29,6 +46,8 @@ jump_counter = 30
 def run_game():
 	global make_Jump
 	game = True
+	cactus_arr = []
+	create_cactus_arr(cactus_arr)
 
 	while game:
 		for event in pygame.event.get():
@@ -45,8 +64,7 @@ def run_game():
 			jump()
 
 		display.fill((255, 255, 255))
-
-		draw_cactus()
+		draw_array(cactus_arr)
 
 		pygame.draw.rect(display, (255, 0, 0), (usr_x, usr_y, usr_width, usr_height))
 
@@ -57,19 +75,20 @@ def jump():
 	global usr_y, jump_counter, make_Jump
 	if jump_counter >= -30:
 		usr_y -= jump_counter / 2.5 
-		jump_counter -=1 
+		jump_counter -= 1 
 	else:
 		jump_counter = 30 
 		make_Jump = False 
 
-def draw_cactus():
-	global cactus_x, cactus_y, cactus_width, cactus_height 
+def create_cactus_arr(array):
+	array.append(Cactus(display_width - 50, display_height - 170, 20, 70, 4))
+	array.append(Cactus(display_width + 50, display_height - 150, 30, 50, 4))
+	array.append(Cactus(display_width + 50, display_height - 180, 25, 80, 4)) 
 
-	if cactus_x >= -cactus_width:
-		pygame.draw.rect(display, (0, 255, 0), (cactus_x, cactus_y, cactus_width, cactus_height))
-		cactus_x -= 4
+def draw_array(array):
 
-	else:
-		cactus_x = display_width - 50
+	for Cactus in array:
+		Cactus.move()
+
 
 run_game()
